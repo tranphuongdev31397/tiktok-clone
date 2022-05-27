f79d1e91f735bff7ef01189aa59d33ecf39ab6bc
Phuong Minh Tran <tranphuongdev31397@gmail.com>
2022-05-27 09:30:11 +0700

    hook useDebounce

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+
+function useDebounce(value, time = 500) {
+    const [latestValue, setLatestValue] = useState(value);
+    useEffect(() => {
+        const idTimeOut = setTimeout(() => {
+            setLatestValue(value);
+        }, time);
+
+        return () => {
+            clearTimeout(idTimeOut);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [value]);
+
+    return latestValue;
+}
+
+export default useDebounce;
