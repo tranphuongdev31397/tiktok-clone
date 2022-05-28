e156c4b712f814e049a506292df4b41d885b0af0
Phuong Minh Tran <tranphuongdev31397@gmail.com>
2022-05-28 17:32:46 +0700

    add fakeUser(defaultUser)

--- src/components/AccountItem/index.jsx
+++ src/components/AccountItem/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,11 +6,12 @@ import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 import Image from '~/components/Image';
 import { Link } from 'react-router-dom';
 import routesConfig from '~/config/routes';
+import { fakeUser } from '~/assets/images';
 
 const cx = classNames.bind(styles);
-function AccountItem({ user }) {
+function AccountItem({ user = fakeUser, className }, ref) {
     return (
-        <Link to={routesConfig.userDetail(user.nickname)} className={cx('wrapper')}>
+        <Link to={routesConfig.userDetail(user.nickname)} className={cx('wrapper', [className])} ref={ref}>
             <Image className={cx('avatar')} alt="accountAvatar" src={user.avatar} />
 
             <div className={cx('information')}>
@@ -18,9 +19,11 @@ function AccountItem({ user }) {
                     <span className={cx('accountId')}>
                         {user.first_name} {user.last_name}
                     </span>
-                    <span>
-                        <FontAwesomeIcon icon={faCircleCheck} className={cx('icon-check')} />
-                    </span>
+                    {user.tick && (
+                        <span>
+                            <FontAwesomeIcon icon={faCircleCheck} className={cx('icon-check')} />
+                        </span>
+                    )}
                 </h5>
                 <p className={cx('name')}>{user.nickname}</p>
             </div>
@@ -28,4 +31,4 @@ function AccountItem({ user }) {
     );
 }
 
-export default AccountItem;
+export default forwardRef(AccountItem);
