e9b62227473ce6e692772393cbc2f196308c6b20
Phuong Minh Tran <tranphuongdev31397@gmail.com>
2022-06-05 17:34:47 +0700

    Component DefaultContent

---
+++ src/components/Popup/DefaultContent/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import classNames from 'classnames/bind';
+import styles from './DefaultContent.module.scss';
+
+const cx = classNames.bind(styles);
+
+function DefaultContent({ className, title, children }) {
+    return (
+        <div
+            className={cx('wrapper', {
+                [className]: className,
+            })}
+        >
+            <p className={cx('title', 'text-default')}>{title}</p>
+            <div className={cx('content')}>{children}</div>
+        </div>
+    );
+}
+
+export default DefaultContent;
