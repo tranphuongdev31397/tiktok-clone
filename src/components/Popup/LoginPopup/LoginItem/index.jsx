d68b0a8d2a2ca77abd9c0b6ae6f63260864462cf
Phuong Minh Tran <tranphuongdev31397@gmail.com>
2022-06-11 11:57:56 +0700

    fix css LoginItem

--- src/components/Popup/LoginPopup/LoginItem/index.jsx
+++ src/components/Popup/LoginPopup/LoginItem/index.jsx
@@ -6,7 +6,9 @@ const cx = classNames.bind(styles);
 function LoginItem({ icon, text, ...props }) {
     return (
         <div className={cx('login__item')} {...props}>
-            <span className={cx('icon', 'flex-center')}>{icon}</span>
+            <span className={cx('icon__box', 'flex-center')}>
+                <div className={cx('icon', 'flex-center')}>{icon}</div>
+            </span>
             <span className={cx('text', 'flex-center')}>{text}</span>
         </div>
     );
